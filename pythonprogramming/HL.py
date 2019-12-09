2a3b0dd527426c4387116e9e4042d9bb63443060
chukimmuoi <chukimmuoi@gmail.com>
2019-12-10 05:13:27 +0700

    Add: HL fix code

--- pythonprogramming/HL.py
+++ pythonprogramming/HL.py
@@ -25,7 +25,9 @@ def lol(parent="50KD"):
     df = pd.DataFrame(columns=['start',
                                'end',
                                'time',
-                               'state'])
+                               'state',
+                               'time_out_wb',
+                               'other'])
 
     for each_dir in stock_list[1:]:
         each_file = os.listdir(each_dir)
@@ -41,20 +43,27 @@ def lol(parent="50KD"):
                 startTime = ""
                 endTime = ""
                 if len(starts) >= 2:
-                    startTime = starts[0].replace("[", "").replace("]", "").replace(" ", "").replace("===========Starting=============", "").replace("\n", "")
+                    startTime = starts[0].replace("[", "").replace("]", "").replace(" ", "").replace(
+                        "===========Starting=============", "").replace("\n", "")
                     ends01 = starts[1].split("FACT0001")
                     if len(ends01) >= 2:
                         ends02 = ends01[1].split("Receive message[ OK]")
                         if len(ends02) >= 2:
                             endTime = ends02[0].replace("[", "").replace("]", "").replace(" ", "").replace("\n", "")
 
-
                 print("start", startTime)
                 print("end", endTime)
                 dis = 0
                 state = "Fail"
+                timeOutWb = 0
+                other = 0
                 if not endTime:
                     state = "Fail"
+                    timeouts = source.split('WB Adjust - timeout')
+                    if len(timeouts) >= 2:
+                        timeOutWb = 1
+                    else:
+                        other = 1
                 else:
                     state = "Pass"
                     startTimes = startTime.split(":")
@@ -65,7 +74,9 @@ def lol(parent="50KD"):
                 df = df.append({'start': startTime,
                                 'end': endTime,
                                 'time': dis,
-                                'state': state}, ignore_index=True)
+                                'state': state,
+                                'time_out_wb': timeOutWb,
+                                'other': other}, ignore_index=True)
 
     save = parent + '.csv'
     df.to_csv(save)
